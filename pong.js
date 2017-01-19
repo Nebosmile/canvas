12c700f8ecbc05c076b9013fdadd3752369b46c0
Akulov Pavel <akulov_pavel88@mail.ru>
2017-01-19 13:00:14 +0200

    PONG INIT

---
+++ pong.js
@@ -0,0 +1,13 @@
+function init() {
+    canvas = document.getElementById('pong');
+    canvas.width = 480;
+    canvas.height = 320;
+    context = canvas.getContext('2d');
+    draw();
+}
+
+function draw() {
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, 480, 320);
+}
+init();
